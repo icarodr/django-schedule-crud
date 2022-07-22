44a26ac432bc47faf7b3bebb34b681dfde576048
icarodr <it46009@gmail.com>
2022-07-22 11:23:49 -0300

    changes on views.py

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from home.models import Agenda
 def index(request):
-    return render(request, 'home/index.html')
+    dados = Agenda.objects.all()
+    context = {
+        'agenda':dados
+    }
+    return render(request, 'home/index.html', context)
+
